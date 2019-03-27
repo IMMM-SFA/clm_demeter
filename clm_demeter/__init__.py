0b6ce2200e38ffcc85b005820fca6d19f9b3fc41
crvernon <chrisrvernon@gmail.com>
2019-03-27 12:27:44 -0400

    fix typo in init

--- clm_demeter/__init__.py
+++ clm_demeter/__init__.py
@@ -1,5 +1,5 @@
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution('demeter_clm').version
+__version__ = pkg_resources.get_distribution('clm_demeter').version
 
 __all__ = ['reclassify_base', 'reclassify_projected']
