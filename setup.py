8ef0fa57782dd9c2bc81383804d26439e8c9481d
crvernon <chrisrvernon@gmail.com>
2019-03-27 12:30:17 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='clm_demeter',
+    version='0.0.1',
+    packages=['clm_demeter'],
+    url='https://github.com/IMMM-SFA/clm_demeter.git',
+    license='BSD2-Clause',
+    author='Chris R. Vernon',
+    author_email='chris.vernon@pnnl.gov',
+    description='Preparatory code to integrate CLM and Demeter'
+)
