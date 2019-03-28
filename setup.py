57e4062c225728937cb77df5fc42329820deca18
crvernon <chrisrvernon@gmail.com>
2019-03-28 17:22:56 -0400

    add reqs to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+__author__ = 'Chris R. Vernon'
+__email__ = 'chris.vernon@pnnl.gov'
+__copyright__ = 'Copyright (c) 2017, Battelle Memorial Institute'
+__license__ = 'BSD 2-Clause'
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().split()
+
 
 setup(
     name='clm_demeter',
     version='0.0.1',
-    packages=['clm_demeter'],
+    packages=find_packages(),
     url='https://github.com/IMMM-SFA/clm_demeter.git',
     license='BSD2-Clause',
     author='Chris R. Vernon',
     author_email='chris.vernon@pnnl.gov',
-    description='Preparatory code to integrate CLM and Demeter'
+    description='Preparatory code to integrate CLM and Demeter',
+    long_description=readme(),
+    install_requires=get_requirements(),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4'
 )
